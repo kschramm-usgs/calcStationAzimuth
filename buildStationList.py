a6fd71a8acd128bfbbc560ff357e5f68d5665cd4
Kimberly Schramm <kschramm@usgs.gov>
2017-03-09 15:39:33 -0700

    add routine to build a station list

---
+++ buildStationList.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+
+'''This will build a list of stations for a given network'''
+
+from obspy.clients.fdsn import Client
+from obspy import UTCDateTime
+
+client = Client("IRIS")
+
+# set some event info
+# this is a deep earthquake in bolivia
+t = UTCDateTime("2017-02-21T14:09:04")
+
+inventory = client.get_stations(network="IU", station="*", starttime=t)
+
+# to plot up all the stations...
+#inventory.plot()
+
+# to get the station coordinatesinformation
+station_coordinates = []
+for network in inventory:
+    for station in network:
+        station_coordinates.append((network.code, station.code, station.latitude, station.longitude, station.elevation))
